7e6939c5b6660fe33e42b9d460b7d5306a67a84c
aliza zahid <alizayzah11@gmail.com>
2023-09-01 22:39:20 +0500

    profile.js

--- profile.js
+++ profile.js
@@ -1,14 +1,19 @@
+const app = firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
 
-const profileContainer = document.querySelector(".profile-container");
-const profileImage = profileContainer.querySelector(".profile-image");
-const editProfileNameIcon = profileContainer.querySelector(".profile-name .edit-icon");
-const passwordForm = profileContainer.querySelector(".password-form");
-const updatePasswordButton = profileContainer.querySelector(".update-button");
+const profileForm = document.getElementById("profile-form");
+const photoInput = document.getElementById("photo-input");
+const firstNameInput = document.getElementById("profile-name");
+const passwordInput = document.getElementById("password-form");
+const editNameIcon = document.getElementById("edit-icon");
 
-// Function to update profile photo
-profileImage.addEventListener("change", async (e) => {
-  const photoFile = e.target.files[0];
+
+// Function to update profile information
+profileForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  // Update profile photo
+  const photoFile = photoInput.files[0];
   if (photoFile) {
     const storageRef = storage.ref(`profile_photos/${photoFile.name}`);
     await storageRef.put(photoFile);
@@ -16,20 +21,54 @@ profileImage.addEventListener("change", async (e) => {
     // Update user's profile photo URL in the database
     // Example: You might use Firebase Firestore or Realtime Database
   }
+
+  // Update first name, last name, and password
+  const firstName = firstNameInput.value;
+  // const lastName = lastNameInput.value;
+  const newPassword = passwordInput.value;
+
+  // Update user information in the database
+  // Example: You might use Firebase Firestore or Realtime Database
+
+  // Clear password input for security
+  passwordInput.value = "";
+
+  alert("Profile updated successfully!");
 });
 
-// Function to toggle profile name editing
-editProfileNameIcon.addEventListener("click", () => {
-  // Toggle the editing state of the profile name
-  // You might implement the editing functionality here
+// Add an event listener to the edit name icon
+editNameIcon.addEventListener("click", () => {
+  // Show a form or modal for editing the name
+  const newName = prompt("Enter your new name:");
+  if (newName !== null) {
+    // Update the displayed name and send it to the database
+    firstNameInput.textContent = newName;
+    // You should also send the new name to the database here
+  }
 });
+const editPhotoIcon = document.getElementById("edit-photo");
 
-// Function to update password
-updatePasswordButton.addEventListener("click", () => {
-  const oldPassword = passwordForm[0].value;
-  const newPassword = passwordForm[1].value;
-  const repeatPassword = passwordForm[2].value;
+// Add an event listener to the edit photo icon
+editPhotoIcon.addEventListener("click", () => {
+  // Create an input element for file upload
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
 
-  // Validate oldPassword, newPassword, and repeatPassword
-  // Update user's password using Firebase Authentication or other methods
+  // Add an event listener to the file input
+  fileInput.addEventListener("change", async (event) => {
+    const photoFile = event.target.files[0];
+
+    if (photoFile) {
+      const storageRef = storage.ref(`profile_photos/${photoFile.name}`);
+      await storageRef.put(photoFile);
+      const photoUrl = await storageRef.getDownloadURL();
+
+      // Update the profile image source and send the new URL to the database
+      photoInput.src = photoUrl;
+      // You should also send the new photo URL to the database here
+    }
+  });
+
+  // Trigger a click event on the file input to open the file selection dialog
+  fileInput.click();
 });
